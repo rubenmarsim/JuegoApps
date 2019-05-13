349ed7571621c211c198aa1a148fe00b8e368277
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-05-13 17:57:18 +0200

    .

---
+++ JuegoDinosaurio/Assets/sPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sPlayer : MonoBehaviour
+{
+    public int iJump;
+    public int iMovSpeed;
+    bool IsEnElPiso = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(iMovSpeed, this.GetComponent<Rigidbody2D>().velocity.y);
+
+        if (Input.GetKeyDown("space")&&IsEnElPiso)
+        {
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, iJump));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IsEnElPiso = true;
+        if (collision.tag.Equals("Obstaculo"))
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsEnElPiso = false;
+    }
+
+}
---
+++ JuegoDinosaurio/Assets/sSceneControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sSceneControl : MonoBehaviour
+{
+    public GameObject Player;
+    public Camera GameCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player != null)
+        {
+            GameCamera.transform.position = new Vector3(Player.transform.position.x,
+                                                    GameCamera.transform.position.y,
+                                                    GameCamera.transform.position.z
+                                                    );
+        }        
+    }
+}
